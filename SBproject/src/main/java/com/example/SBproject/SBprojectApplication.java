c745fd5b31eb8b2d3a623a272084a67fa5e43a01
Brandon <btcossin@gmail.com>
2022-08-18 15:11:41 -0400

    form frame

--- SBproject/src/main/java/com/example/SBproject/SBprojectApplication.java
+++ SBproject/src/main/java/com/example/SBproject/SBprojectApplication.java
@@ -1,31 +1,49 @@
 package com.example.SBproject;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
 
 @SpringBootApplication
 public class SBprojectApplication {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		ButtonOne b1 = new ButtonOne();
 		ButtonTwo b2 = new ButtonTwo();
 
-		Database db = new Database();
-
-		JFrame frame = new JFrame("First GUI");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,500);
-		frame.setVisible(true);
-
-		JButton button1 = new JButton("Button1");
+		Database db = new Database();	
+		//Username
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		JLabel userlabel = new JLabel("Username");
+		panel.add(userlabel);
+		JTextField field = new JTextField(10);
+		userlabel.setLabelFor(field);
+		panel.add(field);
+		//First name
+		JLabel firstlabel = new JLabel("First name");
+		panel.add(firstlabel);
+		JTextField field2 = new JTextField(10);
+		userlabel.setLabelFor(field2);
+		panel.add(field2);
+		//Last name
+		JLabel lastlabel = new JLabel("Last name");
+		panel.add(lastlabel);
+		JTextField field3 = new JTextField(10);
+		userlabel.setLabelFor(field3);
+		panel.add(field3);
+		//Add button
+		JButton button1 = new JButton("Submit");
 		button1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				b1.getInfo();
@@ -39,10 +57,21 @@ public class SBprojectApplication {
 			}
 		});
 		
-		frame.getContentPane().add(BorderLayout.WEST,button1);
-		frame.getContentPane().add(BorderLayout.EAST,button2);
+		panel.add(button1);
+		//frame.getContentPane().add(BorderLayout.EAST,button2);
 
-		db.getName();
+		//db.Test();
+		JFrame frame = new JFrame("First GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,500);
+		frame.setVisible(true);
+		//Set up the content pane.
+        panel.setOpaque(true);  //content panes must be opaque
+        frame.setContentPane(panel);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
 
 		SpringApplication.run(SBprojectApplication.class, args);
 	}
