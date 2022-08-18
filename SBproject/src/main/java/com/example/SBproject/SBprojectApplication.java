8fb193bd817a700fbbe720ea78826173d3c12c2a
skanner2 <63942471+skanner2@users.noreply.github.com>
2022-08-18 11:07:37 -0400

    buttons

--- SBproject/src/main/java/com/example/SBproject/SBprojectApplication.java
+++ SBproject/src/main/java/com/example/SBproject/SBprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.*;
 import javax.swing.*;
+import java.awt.*;
 
 @SpringBootApplication
 public class SBprojectApplication {
@@ -14,8 +15,12 @@ public class SBprojectApplication {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("First GUI");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300,300);
+		frame.setSize(400,500);
 		frame.setVisible(true);
+		JButton button1 = new JButton("Button1");
+		JButton button2 = new JButton("Button2");
+		frame.getContentPane().add(BorderLayout.WEST,button1);
+		frame.getContentPane().add(BorderLayout.EAST,button2);
 		SpringApplication.run(SBprojectApplication.class, args);
 	}
 
