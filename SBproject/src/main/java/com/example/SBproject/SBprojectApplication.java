a71650a56a27c6f063a1985fdef509d170a6f246
skanner1999 <85247879+skanner1999@users.noreply.github.com>
2022-08-18 09:34:33 -0400

    Add files via upload

---
+++ SBproject/src/main/java/com/example/SBproject/SBprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SBproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBprojectApplication.class, args);
+	}
+
+}
---
+++ SBproject/src/test/java/com/example/SBproject/SBprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
